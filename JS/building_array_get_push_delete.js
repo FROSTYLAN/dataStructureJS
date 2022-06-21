3bb82ae39c289fb723fc901a3835dea19b16069a
FROSTYLAN <castillo089frosty@gmail.com>
2022-06-21 00:53:03 -0500

    delete array#

---
+++ JS/building_array_get_push_delete.js
@@ -0,0 +1,49 @@
+class Array {
+  constructor() {
+    this.length = 0;
+    this.data = {};
+  }
+
+  get(index) {
+    return this.data[index];
+  }
+
+  push(item) {
+    this.data[this.length] = item;
+    this.length++;
+    return this.data;
+  }
+
+  pop() {
+    const lastItem = this.data[this.length - 1];
+    delete this.data[this.length - 1]; // keyword delete;
+    // delete es un operador que elimina la propiedad de un objeto.
+    this.length--;
+    return lastItem;
+  }
+
+  delete(index) {
+    const item = this.data[index];
+    this.shiftIndex(index);
+    return item;
+  }
+
+  shiftIndex(index) {
+    for (let i = index; i < this.length - 1; i++) {
+      this.data[i] = this.data[i + 1];
+    }
+    delete this.data[this.length - 1];
+    this.length--;
+  }
+}
+
+const array = new Array();
+
+array.push("Charles");
+array.push("Valeria");
+array.push("Karis");
+array.push("Sol");
+array.pop();
+array.delete(1);
+
+console.log(array.data);
